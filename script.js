3f495ac12d4bb39ca1f8b6184d44e25deb017ac0
Shelley McHardy <shelley.mchardy@gmail.com>
2021-05-20 07:37:29 -0400

    added bootstrap rows - check for responsiveness

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+// displays current day and date in the header
 const today = document.getElementById("currentDay");
-
-let a = moment().format('LLLL');
-
+let a = moment().format('dddd, LL');
 today.textContent = a;
 
 
+let time = moment().format('LT');
+console.log(time);
+
+let checkTime = moment("2021-05-19 16")
+console.log(checkTime);
+console.log(time.isAfter(checkTime));
 
 
+var beginningTime = moment('8:45am', 'h:mma');
+var endTime = moment('9:00am', 'h:mma');
+console.log(beginningTime.isBefore(endTime)); // true
