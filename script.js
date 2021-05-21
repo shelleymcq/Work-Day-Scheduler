32e06332e0a56d4cd3976b0cebe8ffd24442f313
Shelley McHardy <shelley.mchardy@gmail.com>
2021-05-21 13:39:53 -0400

    changed to bootstrap inputs and used jQuery to clone

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 // displays current day and date in the header
-const today = document.getElementById("currentDay");
-let a = moment().format('dddd, LL');
-today.textContent = a;
+const today = moment().format('dddd, LL');
+$("#currentDay").append(today);
 
 
+// displays and updates time - sort of. It changes time every minute, but it starts at min:00 no matter the time...still working on jQuery version
+let clock = function() {
+    document.getElementById("currentTime").innerText = `${moment().format('LT')}`;
+}
+
+clock();
+setInterval(clock, 60000);
+
+
+// clone row with jQuery
+// need a loop to change hour (and color?)
+
+let cloneRow = function() {}
+
+
+
+$(".block").clone().appendTo(".block");
+$("#basic-addon1").text('8:00 AM');
+
+
+
+
+
+
+
+
+// playing with checking current time to style hours
 let time = moment().format('LT');
 console.log(time);
 
