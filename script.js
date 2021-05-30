bc60a7dda76126c5d850ed4065dba9ad615207da
Shelley McHardy <shelley.mchardy@gmail.com>
2021-05-30 15:10:18 -0400

    updating html and js

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
+// .container
+// textarea
+// .row
+// .hour
+// .past
+// .present
+// .future
+// .saveBtn
+
+
 // displays current day and date in the header
 const today = moment().format('dddd, LL');
 $("#currentDay").append(today);
 
 
-// displays and updates time - sort of. It changes time every minute, but it starts at min:00 no matter the time...still working on jQuery version
+// displays and updates time every 10 seconds
 let clock = function() {
     document.getElementById("currentTime").innerText = `${moment().format('LT')}`;
 }
 
 clock();
-setInterval(clock, 60000);
+setInterval(clock, 10000);
 
 
-// gets current hour to compare to row
+let currentHour = moment().format("kk");
+console.log(currentHour);
 
-let time = moment().format("kk");
-
-
-const get08 = $(".hour")[0].id;
-const get09 = $(".hour")[1].id;
-const get10 = $(".hour")[2].id;
-const get11 = $(".hour")[3].id;
-const get12 = $(".hour")[4].id;
-const get13 = $(".hour")[5].id;
-const get14 = $(".hour")[6].id;
-const get15 = $(".hour")[7].id;
-const get16 = $(".hour")[8].id;
-const get17 = $(".hour")[9].id;
-
-let getHour = [get08, get09, get10, get11, get12, get13, get14, get15, get16, get17];
-
-console.log(getHour)
-
-// I really wanted to loop over the getHour array to check time and change color, but couldn't get it to work, and rand out of time before I could finish the save in local storage.
+// querySelectAll returns a node list (all the time blocks) to set color, use jQ (pg 310)
 
 
 
 
+// use sibling to determine which saveBtn is clicked
 
 
 
 
-
+// save input text to local storage
 
 
