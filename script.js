697b8bcf7766718cb8f0eff37f63a2528e3b1c64
Shelley McHardy <shelley.mchardy@gmail.com>
2021-05-30 16:05:47 -0400

    logic for time check and updating style

--- script.js
+++ script.js
@@ -22,11 +22,33 @@ clock();
 setInterval(clock, 10000);
 
 
-let currentHour = moment().format("kk");
+const currentHour = moment().format("kk");
 console.log(currentHour);
 
-// querySelectAll returns a node list (all the time blocks) to set color, use jQ (pg 310)
+const get08 = $(".hour")[0].id;
+const get09 = $(".hour")[1].id;
+const get10 = $(".hour")[2].id;
+const get11 = $(".hour")[3].id;
+const get12 = $(".hour")[4].id;
+const get13 = $(".hour")[5].id;
+const get14 = $(".hour")[6].id;
+const get15 = $(".hour")[7].id;
+const get16 = $(".hour")[8].id;
+const get17 = $(".hour")[9].id;
 
+
+
+$(document).ready(function() {
+    if (get17 === currentHour) {
+        $('.hour#17').addClass('present');
+    } else if (get16 < currentHour) {
+        $('.hour#17').addClass('past');
+    } else {
+        $('.hour#17').addClass('future');
+    }
+});
+
+// need to work on the loop
 
 
 
